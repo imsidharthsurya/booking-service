c79ece1e648f9d60b41a0da19a427a8823dc3fcc
Sidharth Surya <imsidharthsurya@gmail.com>
2024-06-18 19:15:28 +0530

    routes and db sync code added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express=require("express")
+const apiRoutes=require("./routes/index")
 
 const {PORT}=require("./config/serverConfig")
+const db=require("./models/index")
 const app=express();
 
 const prepareAndStartServer=()=>{
 
     app.use(express.json())
+
+    app.use("/api",apiRoutes)
     app.listen(PORT,()=>{
+        if(process.env.DB_SYNC==true){
+            db.sequelize.sync({alter:true})
+        }
         console.log(`server is running on PORT ${PORT}`);
     })
 }
